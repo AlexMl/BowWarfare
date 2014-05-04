3360744347b28a256e925093fffd4ff5cca01dc4
AlexMl <alexandermlinzk@gmail.com>
2014-05-04 14:38:03 +0200

    register Events

--- src/me/Aubli/BowWarfare/BowWarfare.java
+++ src/me/Aubli/BowWarfare/BowWarfare.java
@@ -2,7 +2,13 @@ package me.Aubli.BowWarfare;
 
 import java.util.logging.Logger;
 
+import me.Aubli.BowWarfare.Listeners.EntityDamageListener;
+import me.Aubli.BowWarfare.Listeners.PlayerInteractListener;
+import me.Aubli.BowWarfare.Listeners.PlayerRespawnListener;
+import me.Aubli.BowWarfare.Listeners.SignChangeListener;
+
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BowWarfare extends JavaPlugin{
@@ -23,8 +29,16 @@ public class BowWarfare extends JavaPlugin{
 	}
 	
 	private void init(){
-		
+		registerListeners();
 	}
 	
+	private void registerListeners(){
+		PluginManager pm = Bukkit.getPluginManager();
+		
+		pm.registerEvents(new SignChangeListener(), this);
+		pm.registerEvents(new PlayerRespawnListener(), this);
+		pm.registerEvents(new EntityDamageListener(), this);
+		pm.registerEvents(new PlayerInteractListener(), this);
+	}
 	
 }
