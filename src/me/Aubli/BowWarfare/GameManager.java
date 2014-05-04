f7f28050f577becfcbd8b285cff3074e06915f64
AlexMl <alexandermlinzk@gmail.com>
2014-05-04 15:17:31 +0200

    Added Content

--- src/me/Aubli/BowWarfare/GameManager.java
+++ src/me/Aubli/BowWarfare/GameManager.java
@@ -1,5 +1,52 @@
 package me.Aubli.BowWarfare;
 
-public class GameManager {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+public class GameManager {
+	
+	private File arenaFolder;
+	
+	private ArrayList<BowArena> arenas;
+	
+	
+	public GameManager(){
+				
+		arenaFolder = new File(BowWarfare.getInstance().getDataFolder().getPath() + "/Arenas");
+		
+		load();		
+	}
+	
+	public void shutdown(){
+		save();
+	}
+	
+	
+	private void load(){
+		arenas = new ArrayList<BowArena>();
+		
+		if(!arenaFolder.exists()){
+			arenaFolder.mkdirs();
+		}
+		
+		for(File f : arenaFolder.listFiles()){
+			BowArena a = new BowArena(f);
+			if(a.getWorld()!=null){
+				arenas.add(a);
+			}
+		}
+	}
+	
+	private void save(){
+		
+		try{
+			for(BowArena a : arenas){
+				a.save();
+			}
+			arenas.clear();
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+	}
 }
