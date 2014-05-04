0e96ffd7a18c0441978096d4058f0723708c6d74
AlexMl <alexandermlinzk@gmail.com>
2014-05-04 19:25:17 +0200

    Player content

--- src/me/Aubli/BowWarfare/BowPlayer.java
+++ src/me/Aubli/BowWarfare/BowPlayer.java
@@ -1,5 +1,60 @@
 package me.Aubli.BowWarfare;
 
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
 public class BowPlayer {
 
+	private Player player;
+	private UUID uuid;
+	
+	private int kills;
+	
+	private boolean dead;
+	
+	private BowArena arena;
+	
+	public BowPlayer(Player player, BowArena arena){
+		this.player = player;
+		this.uuid = player.getUniqueId();
+		
+		this.kills = 0;
+		this.dead = false;
+		
+		this.arena = arena;	
+	}
+	
+	
+	public UUID getUuid(){
+		return uuid;
+	}
+	
+	public Player getPlayer(){
+		return player;
+	}
+	
+	public int getKills(){
+		return kills;
+	}
+	
+	public BowArena getArena(){
+		return arena;
+	}
+	
+	
+	public boolean isDead(){
+		return dead;
+	}
+	
+	
+	public void reset(){
+		
+	}
+	
+	public void getReady(){
+		
+	}
+	
+	
 }
