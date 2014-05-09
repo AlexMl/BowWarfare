ba9db2a64eed9ead8c6930d5f947dd3bc8698855
AlexMl <alexandermlinzk@gmail.com>
2014-05-09 22:02:29 +0200

    Added command help and commands

--- src/me/Aubli/BowWarfare/BowExecuter.java
+++ src/me/Aubli/BowWarfare/BowExecuter.java
@@ -13,10 +13,19 @@ public class BowExecuter implements CommandExecutor {
 
 	/*
 	 *	Commands: 
+	 *
+	 *  /bw reload
+	 *
 	 * 	/bw arena pos1
 	 *  /bw arena pos2
 	 *   
-	 * 
+	 *  /bw remove [arenaID]
+	 *  
+	 *  
+	 *  
+	 *  
+	 *  /bw stop [ID]
+	 *  /bw stop
 	 */
 	
 	private HashMap<Integer, Location> pos = new HashMap<Integer, Location>();
@@ -59,11 +68,57 @@ public class BowExecuter implements CommandExecutor {
 		
 		if(cmd.getName().equalsIgnoreCase("bw")){
 			
+			if(args.length==1){
+				if(args[0].equalsIgnoreCase("help")){
+					printCommands(playerSender);
+					return true;
+				}
+				if(args[0].equalsIgnoreCase("reload")){
+					if(playerSender.hasPermission("bw.reload")){
+						GameManager.getManager().reload();
+						playerSender.sendMessage(ChatColor.GREEN + "Config reloaded!");
+						return true;
+					}else{
+						commandDenied(playerSender);
+						return true;
+					}
+				}
+				if(args[0].equalsIgnoreCase("stop")){
+					if(playerSender.hasPermission("bw.stop.all")){
+						GameManager.getManager().stopArenas();
+						playerSender.sendMessage(ChatColor.DARK_GRAY + "[BW] All Arenas were stoped!");
+						return true;
+					}else{
+						commandDenied(playerSender);
+						return true;
+					}
+				}
+				
+				printCommands(playerSender);
+				return true;
+			}
 			
 			
 			if(args.length==2){
+				
+				if(args[0].equalsIgnoreCase("stop")){
+					if(playerSender.hasPermission("bw.stop")){
+						BowArena a = GameManager.getManager().getArena(Integer.parseInt(args[1]));						
+						if(a!=null){
+							GameManager.getManager().stopArena(a);
+							playerSender.sendMessage(ChatColor.DARK_GRAY + "[BW] Arena " + a.getID() + " halted!");
+							return true;
+						}else{
+							playerSender.sendMessage(ChatColor.RED + "Arena is not available!");
+							return true;
+						}
+					}else{
+						commandDenied(playerSender);
+						return true;
+					}
+				}
 				if(playerSender.hasPermission("bw.admin")){
-					if(args[0].equalsIgnoreCase("arena")){					
+					if(args[0].equalsIgnoreCase("arena")){				
 						if(args[1].equalsIgnoreCase("pos1")){
 							pos.put(1, playerSender.getLocation().clone());
 							playerSender.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Position 1 saved!");
@@ -84,7 +139,7 @@ public class BowExecuter implements CommandExecutor {
 								playerSender.sendMessage(ChatColor.RED + "Job failed!");
 								return true;
 							}							
-						}					
+						}
 					}
 					if(args[0].equalsIgnoreCase("remove")){
 						BowArena a = GameManager.getManager().getArena(Integer.parseInt(args[1]));
@@ -102,13 +157,38 @@ public class BowExecuter implements CommandExecutor {
 							return true;
 						}
 					}
+					printCommands(playerSender);
 				}else{
-					playerSender.sendMessage(ChatColor.DARK_RED + "You do not have permissions for that!");
+					commandDenied(playerSender);
 					return true;
 				}				
-			}			
+			}	
+			printCommands(playerSender);
+			return true;
 		}
 		return true;
+	}
+	
+	
+	private void printCommands(Player player){
+		if(player.hasPermission("bw.help")){
+			String version = BowWarfare.getInstance().getDescription().getVersion();
+			String name = BowWarfare.getInstance().getDescription().getName();
+			
+			player.sendMessage(ChatColor.DARK_GREEN + "|–––––––––– " + ChatColor.DARK_GRAY + name + " v" + version + ChatColor.DARK_GREEN + " ––––––––––");
+			player.sendMessage(ChatColor.DARK_GREEN + "| /bw reload");
+			
+			player.sendMessage(ChatColor.DARK_GREEN + "| /bw arena [pos1|pos2]");
+			player.sendMessage(ChatColor.DARK_GREEN + "| /bw remove [Arena-ID]");
+			
+			
+		}else{
+			commandDenied(player);
+		}
+	}
+	
+	private void commandDenied(Player player){
+		player.sendMessage(ChatColor.DARK_RED + "You do not have permissions for that!");
 	}
 
 }
