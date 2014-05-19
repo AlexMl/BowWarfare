8905b1372e9260d41ce0984dadeba7eebb809f4a
AlexMl <alexandermlinzk@gmail.com>
2014-05-19 15:04:54 +0200

    Changed signChange

--- src/me/Aubli/BowWarfare/Listeners/SignChangeListener.java
+++ src/me/Aubli/BowWarfare/Listeners/SignChangeListener.java
@@ -17,33 +17,28 @@ public class SignChangeListener implements Listener{
 	public void onSignChange(SignChangeEvent event){
 		
 		if(event.getLine(0).equalsIgnoreCase("[bw]")){
-			if(!event.getLine(1).isEmpty() && !event.getLine(2).isEmpty()){
+			if(!event.getLine(1).isEmpty()){
 				if(event.getPlayer().hasPermission("bw.admin")){
-					if(event.getLine(1).equalsIgnoreCase("join")){
-						try{
-							int arenaID = Integer.parseInt(event.getLine(2));
-							BowArena arena = GameManager.getManager().getArena(arenaID);
-							if(arena!=null){
-								SignManager.getManager().createSign(arena, event.getBlock().getLocation());
+					try{
+						int arenaID = Integer.parseInt(event.getLine(1));
+						BowArena arena = GameManager.getManager().getArena(arenaID);
+						if(arena!=null){
+							SignManager.getManager().createSign(arena, event.getBlock().getLocation());
+							
+							event.setLine(0, ChatColor.DARK_BLUE.toString() + "Bow Warfare");
+							event.setLine(1, ChatColor.LIGHT_PURPLE + "[JOIN]");
+							event.setLine(2, ChatColor.GREEN + "" + arena.getPlayers().length + " / " + BowWarfare.getMaxPlayers());
+							event.setLine(3, ChatColor.DARK_RED + "Arena " + arena.getID());
 								
-								event.setLine(0, ChatColor.DARK_BLUE.toString() + "Bow Warfare");
-								event.setLine(1, ChatColor.LIGHT_PURPLE + "[JOIN]");
-								event.setLine(2, ChatColor.GREEN + "" + arena.getPlayers().length + " / " + BowWarfare.getMaxPlayers());
-								event.setLine(3, ChatColor.DARK_RED + "Arena " + arena.getID());
-								
-								event.getPlayer().sendMessage(ChatColor.GREEN + "Sign Created!");
-								return;
-							}else{
-								event.getPlayer().sendMessage(ChatColor.RED + "This arena is not available!");
-								event.setCancelled(true);
-								return;
-							}
-						}catch(NumberFormatException e){
-							event.getPlayer().sendMessage(ChatColor.RED + "Only numbers are acceptable!");
+							event.getPlayer().sendMessage(ChatColor.GREEN + "Sign Created!");
+							return;
+						}else{
+							event.getPlayer().sendMessage(ChatColor.RED + "This arena is not available!");
 							event.setCancelled(true);
 							return;
 						}
-					}else{
+					}catch(NumberFormatException e){
+						event.getPlayer().sendMessage(ChatColor.RED + "Only numbers are acceptable!");
 						event.setCancelled(true);
 						return;
 					}
